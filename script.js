8fb84342daf270486a1fae78aa2214e438cb608d
RussiaBear1337 <163581054+RussiaBear1337@users.noreply.github.com>
2024-05-05 13:08:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-**Тут будет скрипт**
+let rayan = document.querySelector(".rayan")
+let scoreHTML = document.querySelector(".score")
+
+
+
+let score = 0
+
+rayan.addEventListener("click", function(){
+    score = score + 1
+    scoreHTML.innerHTML = score
+    
+})
